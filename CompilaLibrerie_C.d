253ecbe3e0445ea6020e3921870ccc3931074e5c
frassix <86714881+frassix@users.noreply.github.com>
2021-12-28 18:16:46 +0100

    Delete CompilaLibrerie_C.d

--- CompilaLibrerie_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./CompilaLibrerie_C.so: /home/matilde/root/include/RVersion.h /home/matilde/root/include/RConfig.h /home/matilde/root/include/TClass.h /home/matilde/root/include/TDictAttributeMap.h /home/matilde/root/include/TInterpreter.h /home/matilde/root/include/TROOT.h /home/matilde/root/include/TBuffer.h /home/matilde/root/include/TMemberInspector.h /home/matilde/root/include/TError.h /home/matilde/root/include/RtypesImp.h /home/matilde/root/include/TIsAProxy.h /home/matilde/root/include/TFileMergeInfo.h /home/matilde/root/include/TCollectionProxyInfo.h /home/matilde/root/bin/rootcling
-CompilaLibrerie_C__ROOTBUILDVERSION= 6.24/02
