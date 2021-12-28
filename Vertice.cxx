8d7975b83c939c39ff08c009c9ac707ddb7d75fe
MatildeDematteis <95911480+MatildeDematteis@users.noreply.github.com>
2021-12-28 17:03:51 +0100

    Update Vertice.cxx

--- Vertice.cxx
+++ Vertice.cxx
@@ -3,11 +3,11 @@
 ClassImp(Vertice)
 
 //Costruttore di default
-Vertice::Vertice() : Punto(), dmN(0.){}
+Vertice::Vertice() : Punto(), dmN(0){}
 
 //Costruttore standard
 Vertice::Vertice(double x, double y, double z, int N): Punto(x,y,z), dmN(N){
-    dmDir.reserve(N);
+    dmDir.reserve(N); //alloca memoria per il vettore di particelle (grandezza minima allocata nella heap)
 }
 
 //Costruttore di copia
@@ -16,7 +16,7 @@ Vertice::Vertice(const Vertice& source) : Punto(source), dmN(source.dmN){
         std::vector<Particella> dmDir(dmN);
 
         for(int i = 0; i<dmN; i++){
-            dmDir.at(i)=source.dmDir.at(i);
+            dmDir.at(i)=source.dmDir.at(i); //at(i) dà la reference dell'i-esimo elemento del vector 
         }
     }
     else{
@@ -26,7 +26,7 @@ Vertice::Vertice(const Vertice& source) : Punto(source), dmN(source.dmN){
 
 //Distruttore standard
 Vertice::~Vertice(){
-    //Per i vector non serve fare il delete
+    //Per i vector non serve fare il delete, se ne occupa vector una volta fuori dallo scope
 }
 
 //Operatore copia
@@ -39,5 +39,5 @@ Vertice& Vertice::operator=(const Vertice& source){
 
 //Setter di dmDir
 void Vertice::AddPart(double Theta, double Phi){
-    dmDir.push_back(Particella(Theta, Phi));
+    dmDir.push_back(Particella(Theta, Phi)); //push_back aggiuge una particella al fondo del vettore dmDir
 }
