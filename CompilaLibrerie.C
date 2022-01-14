9d3e3c582b12655b6ccb1c493a7cb60e31080a84
MatildeDematteis <95911480+MatildeDematteis@users.noreply.github.com>
2022-01-14 18:28:26 +0100

    Add files via upload

--- CompilaLibrerie.C
+++ CompilaLibrerie.C
@@ -14,6 +14,8 @@ void CompilaLibrerie(TString Opzione="fast"){
 	gSystem->CompileMacro("Rivelatore.cxx",Scelta.Data());
 	gSystem->CompileMacro("MyRandom.cxx",Scelta.Data());
 	gSystem->CompileMacro("Segnale.cxx",Scelta.Data());
+	gSystem->CompileMacro("Tracklet.cxx",Scelta.Data());
 	
 	gSystem->CompileMacro("ALICE_Junior.C",Scelta.Data());
+	gSystem->CompileMacro("Ricostruzione_Vertice.C",Scelta.Data());
 }
---
+++ Ricostruzione_Vertice.C
@@ -0,0 +1,98 @@
+//Librerie custom
+#include "Segnale.h"
+#include "Tracklet.h"
+
+//Librerie
+#include "Riostream.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TMath.h"
+#include "TClonesArray.h"
+#include "TH1D.h"
+
+void Ricostruzione_Vertice(int dim = 36){
+    //dim = 36: dimensione minima dei TClonesArray
+
+    //Costanti
+    double pi_greco = TMath::Pi();
+
+    //Settaggi
+    double r1 = 4.; //cm
+    double r2 = 7.; //cm
+    double delta_phi = (10*pi_greco)/180; //ampiezza angolare in rad entro cui cercare corrispondenza hit
+
+    // definizione struct
+    typedef struct {
+        double x, y, z;
+        int molt;
+    } Vertice;
+
+    static Vertice inizio;
+
+    // Dichiarazione TClonesArray
+    TClonesArray *riv_1 = new TClonesArray("Segnale",dim);
+    TClonesArray *riv_2 = new TClonesArray("Segnale",dim);
+ 
+    //Apertura file di input
+    TFile Input_file("MonteCarlo.root");
+
+    //Lettura TTree e branch
+    TTree *tree = (TTree*)Input_file.Get("Tree");
+    TBranch *b1 = tree->GetBranch("VertMult");
+    TBranch *b2 = tree->GetBranch("Hit1");
+    TBranch *b3 = tree->GetBranch("Hit2");
+
+    // Definizione degli indirizzi per la lettura dei dati su ttree
+    b1->SetAddress(&inizio.x);
+    b2->SetAddress(&riv_1);
+    b3->SetAddress(&riv_2);
+
+    //Creiamo un tracklet, con r1 ed r2 fissati
+    Tracklet* tr = new Tracklet(r1, r2, 0., 0.); //Primo punto: layer 1; Secondo punto: layer 2
+
+    //Creiamo un istogramma per le z
+    TH1D* histo_z = new TH1D("histo_z", "Istogramma delle z", 150, -21.2, 21.2); //4 sigma a destra e a sinistra
+
+    // loop sugli ingressi nel TTree
+    for(int i=0; i<tree->GetEntries(); i++){
+        tree->GetEvent(i);
+        /*
+        std::cout << "Evento " << i+1 << "; Molteplicita= " << inizio.molt << std::endl;
+        std::cout << "X,Y,Z = " << inizio.x << "; " << inizio.y << "; " << inizio.z << std::endl;
+
+        int num = riv_1->GetEntries();
+        std::cout << "Numero di elementi nel primo TClonesArray " << num << std::endl;
+        for (int j=0; j<num; j++){
+            Segnale *tst=(Segnale*)riv_1->At(j);
+            std::cout << "Segnale " << j+1 << ") z, phi = " << tst->GetZ() << "; " << tst->GetPhi() << std::endl;
+        }
+
+        num = riv_2->GetEntries();
+        std::cout << "Numero di elementi nel secondo TClonesArray " << num << std::endl;
+        for (int j=0; j<num; j++){
+            Segnale *tst=(Segnale*)riv_2->At(j);
+            std::cout << "Segnale " << j+1 << ") z, phi = " << tst->GetZ() << "; " << tst->GetPhi() << std::endl;
+        }
+        */
+
+       for(int j=0; j<riv_1->GetEntries(); j++){ //for sul layer 1
+            Segnale* interazione1 = (Segnale*)riv_1->At(j);
+            tr->SetZ1(interazione1->GetZ());
+
+            for(int k=0; k<riv_2->GetEntries(); k++){  //for sul layer 2
+                Segnale* interazione2 = (Segnale*)riv_2->At(k);
+
+                if( (interazione1->GetPhi() - interazione2->GetPhi()) <= delta_phi ){
+                    tr->SetZ2(interazione2->GetZ());
+                    histo_z->Fill(tr->Intersezione());
+                }
+           }
+
+       }
+
+       //Reset dell'istogramma 
+       histo_z->Reset();
+    }
+
+}
