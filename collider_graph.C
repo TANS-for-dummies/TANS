d1dda192cae05dc75ae101e8687621f73dd97e1f
frassix <86714881+frassix@users.noreply.github.com>
2022-03-31 22:21:14 +0200

    Update collider_graph.C

--- collider_graph.C
+++ collider_graph.C
@@ -1,9 +1,41 @@
-import "TGLViewer.h"
-
+#include "TGLViewer.h"
+#include "Segnale.h"
+#include "Riostream.h"
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TMath.h"
+#include "TStopwatch.h"
+#include "TClonesArray.h"
+#include "TStyle.h"
 
 
 void collider_graph()
 {
+  TClonesArray *riv_1 = new TClonesArray("Segnale",dim);
+  TClonesArray *riv_2 = new TClonesArray("Segnale",dim);
+  TFile Input_file(input);
+  
+  std::cout << "Evento da studiare:" << std::endl;    
+	std::cin >> N;
+  
+  TObject *obj = (TObject*)Input_file.Get("Generazione");
+    
+  int N_molt = obj -> GetUniqueID();
+  
+  TTree *tree = (TTree*)Input_file.Get("Tree");
+  TBranch *b1 = tree->GetBranch("VertMult");
+  TBranch *b2 = tree->GetBranch("Hit1");
+  TBranch *b3 = tree->GetBranch("Hit2");
+  b1->SetAddress(&inizio.x);
+  b2->SetAddress(&riv_1);
+  b3->SetAddress(&riv_2);
+
+  tree->GetEvent(i)
+  
+  
+  
   TCanvas *c1 = new TCanvas();
   
   TGLViewer *view = (TGLViewer*)gPad->GetViewer3D(); //accede al pad del canvas
@@ -17,9 +49,15 @@ void collider_graph()
   
   TGeoVolume *beam_pipe = man->MakeTube("PIPE", NULL, 3., 3.08, 27.); //al posto di null potrei creare un medium che registri automaticamente gli hit delle particelle....
   beam_pipe->SetLineColor(kGreen);
+  TGeoVolume *riv_1 = man->MakeTube("RIV1", NULL, 4., 4.02, 27.);
+  riv_1->SetLineColor(kOrange);
+  TGeoVolume *riv_2 = man->MakeTube("RIV1", NULL, 7., 7.02, 27.);
+  riv_2->SetLineColor(kBlue);
   
   man->SetTopVolume(top); //setto il volume madre (esterno)
   top->AddNode(beam_pipe, 0, trans_rot); //creo il layer figlio 0-esimo
+  top->AddNode(riv_1, 1, trans_rot);
+  top->AddNode(riv_2, 2, trans_rot);
   
   man->CloseGeometry(); //chiudere sempre la geometria creata se no non disegna
   top->Draw("ogl"); //ogl crea l'effettivo oggetto con raytracing ecc, quindi può essere pesante
